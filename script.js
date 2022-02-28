71efb9ba4416eec6b408e50050e314fa9adf349c
an-agha <anaghanisakar@gmail.com>
2022-02-28 16:05:26 +0530

    responsive site first commit

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ function showItem(){
     var para = document.getElementById("nav-show");
         para.classList.toggle("paragraphClass");
 }
+
+window.onscroll=function(){
+    if(window.pageYOffset>200){
+        document.getElementById("logo").style.backgroundColor="#7D83FF";
+        document.getElementById("nav-show").style.backgroundColor="#7D83FF";
+           
+    }
+    else{
+        document.getElementById("logo").style.backgroundColor="transparent";
+        document.getElementById("nav-show").style.backgroundColor="transparent";
+    }
+    }
