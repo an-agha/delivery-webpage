354943821565a9dc13a40c8d13349896f6374b14
an-agha <anaghanisakar@gmail.com>
2022-03-16 14:41:01 +0530

    nav bar

--- script.js
+++ script.js
@@ -3,14 +3,7 @@ function showItem(){
         para.classList.toggle("paragraphClass");
 }
 
-window.onscroll=function(){
-    if(window.pageYOffset>200){
-        document.getElementById("logo").style.backgroundColor="#7D83FF";
-        document.getElementById("nav-show").style.backgroundColor="#7D83FF";
-           
-    }
-    else{
-        document.getElementById("logo").style.backgroundColor="transparent";
-        document.getElementById("nav-show").style.backgroundColor="transparent";
-    }
-    }
+
+function close(){
+    document.getElementById("nav-show").style.left ="-100%";
+}
